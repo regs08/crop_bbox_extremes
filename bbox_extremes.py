79c9113c89b07ca7b937dc9443c5377bda8e1fe3
cregs08 <cole.regnier@gmail.com>
2023-06-20 21:24:07 -0300

    first upload

---
+++ bbox_extremes.py
@@ -0,0 +1,73 @@
+def get_bbox_extreme_with_min_pixel_value(bboxes, min_pixel_value):
+    bbox_extremes = get_bbox_extremes(bboxes)
+    bbox_extremes_with_min_pixel_value = check_bbox_extremes_for_min_pixel_value(bbox_extremes, min_pixel_value)
+    return bbox_extremes_with_min_pixel_value
+
+
+def check_bbox_extremes_for_min_pixel_value(bbox, min_pixel_value):
+    # Calculate the width and height of the cropped image
+    min_x, min_y, max_x, max_y = bbox
+    width = max_x - min_x
+    height = max_y - min_y
+
+    # Check if the width and height of the cropped image meet the minimum pixel value
+    if width < min_pixel_value:
+        min_x -= (min_pixel_value - width) // 2
+        max_x += (min_pixel_value - width + 1) // 2
+    if height < min_pixel_value:
+        min_y -= round(min_pixel_value - height) // 2
+        max_y += round(min_pixel_value - height + 1) // 2
+    out = [int(min_x), int(min_y), int(max_x), int(max_y)]
+    for i, x in enumerate(out):
+        if x < 0:
+            out[i] = 0
+
+    return out
+
+
+def get_bbox_extremes(bboxes):
+    """
+    Given a list of bounding boxes (xmin, ymin, xmax, ymax), return the lowest xmin and ymin and highest xmax and ymax
+    across all the bounding boxes.
+
+    Args:
+    - bboxes: list of tuples representing bounding boxes in the format (xmin, ymin, xmax, ymax)
+
+    Returns:
+    - Tuple containing (lowest xmin, lowest ymin, highest xmax, highest ymax)
+    """
+    min_x = min([bbox[0] for bbox in bboxes])
+    min_y = min([bbox[1] for bbox in bboxes])
+    max_x = max([bbox[2] for bbox in bboxes])
+    max_y = max([bbox[3] for bbox in bboxes])
+    box = (min_x, min_y, max_x, max_y)
+
+    return box
+
+
+def get_split_intervals(start, end, interval):
+    """
+    getting split points for an image, usually start will be equal to 0 and end
+    will be the images width.
+
+    In our case we are starting where the bboxes start and endning where they end.
+    """
+    # start is the minimum value
+    # end is the width of the image gotten from our bbox extreme
+
+    # if theres a remainder we subtract the last line
+    num_lines = (end // interval) - 1 if (end // interval) > 0 else 0
+    # for the first value of start for our splits
+    intervals = [start]
+
+    # Draw vertical lines at every n number of pixels until step > end
+    if num_lines > 0:
+        x = interval + start
+        for i in range(num_lines):
+            intervals.append(x)
+            x += interval
+            if x + interval > end:
+                break
+    # appending image with for the last split
+    intervals.append(end)
+    return intervals
---
+++ crop.py
@@ -0,0 +1,136 @@
+import numpy as np
+import supervision as sv
+from crop_bbox_extremes.bbox_extremes import *
+from crop_bbox_extremes.utils import *
+import albumentations as A
+import cv2
+
+"""
+functions for splitting images based on bbox extremes and min pixel values
+"""
+
+
+# we go from dataset to image to split. image to dict to dataset
+def split_images_via_bbox_extremes_and_interval(dataset,
+                                                min_boxes=1,
+                                                interval=1280,
+                                                min_pixel_value=1280):
+    """
+    takes in an dataset splits the images vertically using the points provided
+    by the intervals. intervals are obtained by getting the bbox extremes and the min pixel params
+    :param image_folder: location of images
+    :param interval: interval for the split
+    :param save_folder: save folder for the cropped images
+    :param ann_folder: if theres a different folder for annotations..
+    :param save: save the images
+    :param min_boxes: check so that all images have an instance present
+    :return:
+    """
+    filenames = list(dataset.images.keys())
+    output = {}
+    for filename in filenames:
+        #info from our dataset
+        img = dataset.images[filename]
+        ann = dataset.annotations[filename]
+        class_ids = ann.class_id
+        bboxes = ann.xyxy
+
+        # instead of loading in the text file we can just load in bbox extremes here
+        xmin, ymin, xmax, ymax = get_bbox_extreme_with_min_pixel_value(bboxes, min_pixel_value)
+        if ymin < 0:
+            ymin = 0
+
+        # getting the coords for our vertical split for each image
+        split_intervals = get_split_intervals(xmin, xmax, interval)
+        split_image = vertical_split_img_with_intervals(img=img,
+                                                        bboxes=bboxes,
+                                                        class_ids=class_ids,
+                                                        intervals=split_intervals,
+                                                        ymin=ymin,
+                                                        ymax=ymax,
+                                                        min_boxes=min_boxes,
+                                                        filename=filename)
+        # formatting our data for a detection dataset
+        for data in split_image:
+            # casting data as numpy
+            class_id = np.array(data['class_id'])
+            anns = np.array(data['anns'])
+
+            output[data['filename']] = {
+                'image': data['image'],
+                #casting as Detectionms to in order to make sv.Dataset
+                'anns': sv.Detections(xyxy=anns, class_id=class_id),
+            }
+
+    return output
+
+
+def vertical_split_img_with_intervals(img, intervals, bboxes, class_ids, filename, min_boxes, ymin, ymax):
+    """
+    splits the image using the albumentations library the x values are gotten from intervals. the y values can be given
+    their default is 0, hieght of the image
+    :param img: path or arr, if path we add a filename to our dict
+    :param intervals: the poitns where we will crop
+    :param bboxes: the bboxes, default is yolo format, just coords we will add the class label onto the 5th index.
+    :param class_labels: list of the class ids, ints
+    :return: a list of dictionaries containing the split image info
+    """
+
+    # getting the "ValueError: Your 'label_fields' are not valid - them must have same names as params in dict" so getting rid
+    # adding the label on to the end of the box
+    bboxes = [np.append(box, class_ids[i]) for i, box in enumerate(bboxes)]
+    out_images = []
+
+    for i in range(len(intervals) - 1):
+        xmin = intervals[i]
+        xmax = intervals[i + 1]
+        split_coords = [xmin, ymin, xmax, ymax]
+
+        split_image = get_split_image(img=img,
+                                      split_coords=split_coords,
+                                      bboxes=bboxes,
+                                      min_boxes=min_boxes,
+                                      class_ids=class_ids,
+                                      )
+
+        # maybe here just build the dicts
+        if split_image:
+            # adding filename
+            file_no = f'_{i}'
+            # where i is the number of crops per image
+            crop_filename = insert_string_before_extension(filename, file_no)
+            out = {
+                'filename': crop_filename,
+                'anns': split_image['bboxes'],
+                'image': split_image['image'],
+                'class_id': split_image['class_id']
+            }
+            out_images.append(out)
+    # list of lists where each idx is a dict which reperesents an imagethat is split
+    return out_images
+
+
+def get_split_image(img, split_coords, bboxes, class_ids, min_boxes):
+    """
+    split our image and build a dictionary to return
+    :param img: img as array
+    :param xmin: min val dimensions for our split
+    :param xmax: max val
+    :param bboxes: bboxes from the image
+    :param class_labels: class_labels as strs
+    :return: a dict containing , image, bboxes, category_ids
+    """
+    xmin, ymin, xmax, ymax = split_coords
+    aug = A.Compose([
+        A.Crop(x_min=xmin, x_max=xmax, y_min=ymin, y_max=ymax),
+    ], bbox_params=A.BboxParams(format='pascal_voc', min_visibility=0.3))
+
+    vertical_split_image = aug(image=img, bboxes=bboxes, category_ids=class_ids)
+    if len(vertical_split_image['bboxes']) > min_boxes:
+        # gets rid of ourclass id at the end
+        vertical_split_image['class_id'] = [box[4] for box in vertical_split_image['bboxes']]
+        vertical_split_image['bboxes'] = [box[:4] for box in vertical_split_image['bboxes']]
+        # gets our class id
+        vertical_split_image['image'] = cv2.cvtColor(vertical_split_image['image'], cv2.COLOR_BGR2RGB)
+        return vertical_split_image
+    return None
